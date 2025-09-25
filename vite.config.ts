188782375d8a8ddadc6b063b8d1ded00b3a25bf9
lucas rondanorte <lucasrondanorte@gmail.com>
2025-09-24 23:59:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 IMPORTANTE: el nombre del repo entre barras
 export default defineConfig({
   plugins: [react()],
-  base: '/sportclub-crm--prueba-mejoras/',
+  base: '/sportclub-crm--prueba-mejoras/', // 👈 importante
 })
+
