981f1571dae75d7e3a82a92cda54dadbf4ca94df
lucas rondanorte <lucasrondanorte@gmail.com>
2025-10-02 01:40:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/sportclub-crm--prueba-mejoras/', // 👈 importante para GH Pages
   plugins: [react()],
-  base: '/sportclub-crm--prueba-mejoras/', // 👈 importante
 })
 
